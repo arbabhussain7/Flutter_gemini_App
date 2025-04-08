9f96d11d91ddcb12264128e537dd1642985ded7d
arbabhussain7 <arbabhussain414@gmail.com>
2025-04-08 11:46:41 +0500

    initial commit

---
+++ lib/constant/const.dart
@@ -0,0 +1,1 @@
+// String GEMINI_API_KEY = 'AIzaSyD23yh-8Ikhevf8bS4BBtAc_otcu-h5TOE';
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_geminies_app/constant/const.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  // Access the API key from .env file
+  final geminiApiKey = dotenv.env['GEMINI_API_KEY'];
+
+  if (geminiApiKey == null || geminiApiKey.isEmpty) {
+    throw Exception('GEMINI_API_KEY not found in .env file');
+  }
+
+  // Initialize Gemini with the key
+  Gemini.init(apiKey: geminiApiKey);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
